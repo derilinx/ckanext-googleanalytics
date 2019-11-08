5a914dbe9cf32f1ea0c29fb43f49af9a96903a98
gtkChop <swaroopbhatk@gmail.com>
2019-11-08 12:05:54 +0000

    added ckan resource view, dataset view and downlaod event

--- ckanext/googleanalytics/fanstatic_library/googleanalytics_event_tracking.js
+++ ckanext/googleanalytics/fanstatic_library/googleanalytics_event_tracking.js
@@ -6,11 +6,26 @@ this.ckan.module('google-analytics', function(jQuery, _) {
     },
     initialize: function() {
       jQuery("a.resource-url-analytics, a.btn-download").on("click", function() {
-          var resource_url = encodeURIComponent(jQuery(this).prop('href'));
+          var resource_url = jQuery(this).prop('href');
           if (resource_url) {
             ga('send', 'event', 'Resource', 'Download', resource_url);
           }
       });
+   
+      jQuery(".dataset-heading a").on("click", function() {
+          var dataset_url = jQuery(this).prop('href');
+          if (dataset_url) {
+            ga('send', 'event', 'Dataset', 'CKAN_Dataset_view', dataset_url);
+          }
+      });
+
+      jQuery(".dataset-resource-text a:eq(1)").on("click", function() {
+          var resource_url = jQuery(this).prop('href');
+          if (resource_url) {
+            ga('send', 'event', 'Resource', 'CKAN_Resource_view', resource_url);
+          }
+      });
+    
     }
   }
 });
